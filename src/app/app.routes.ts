f132a1cee9efe90073ab82293051b25260132351
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-09-17 12:07:15 +0200

    add chat component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/chat', pathMatch: 'full' },
+  { path: 'chat', loadComponent: () => import('./chat/chat').then((m) => m.Chat) },
+];
